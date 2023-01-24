af315b8149aaa65f666c862a2df5685e0d604bb5
felixDev22 <felixnzomo@gmail.com>
2023-01-24 13:41:43 +0300

    Implement ducks pattern, add books and remove-book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,33 +1,29 @@
+import { createReducer } from '@reduxjs/toolkit';
 
 // Define action type for addding & removing
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-// set intial state to an empty array of books
-const initialState = {
-  books: [
-    {
-      id: 1,
-      title: 'Maasai Mara',
-      author: 'Bazu Bazu',
-      category: 'Adventure',
-    },
-    {
-      id: 2,
-      title: 'Giraffe Park',
-      author: 'Sonko Bazu',
-      category: 'Comedy',
-    },
-    {
-      id: 3,
-      title: 'Amboseli Mambo',
-      author: 'Kitu Safi',
-      category: 'Romance',
-    },
-  ],
-};
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
 
-// Reducer
-const booksReducer = (state = initialState, action = {}) => {
-  if( act)
+export const removeBook = (bookId) => ({
+  type: ADD_BOOK,
+  payload: bookId,
+});
+
+// set intial state to an empty array of books
+const initialState = [];
+
+const booksReducer = createReducer(initialState, {
+  [ADD_BOOK]: (state, action) => {
+    state.push(action.payload);
+  },
+  [REMOVE_BOOK]: (state, action) => {
+    state.filter((book) => book.id !== action.payload);
+  },
+});
+
 export default booksReducer;
