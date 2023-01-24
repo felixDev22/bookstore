bdc5fed0992aaae280466b87e91e85e8d001a052
felixDev22 <felixnzomo@gmail.com>
2023-01-24 03:23:53 +0300

    Add redux-categories.js file,Add check status action type

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,12 +1,15 @@
 import { createReducer, createAction } from '@reduxjs/toolkit';
 
+// Define action type for addding & removing
 export const ADD_BOOK = createAction('books/add');
 export const REMOVE_BOOK = createAction('books/remove');
 
+// set intial state to an empty array of books
 const initialState = {
   books: [],
 };
 
+// Reducer
 const booksReducer = createReducer(initialState, (builder) => {
   builder.addCase(ADD_BOOK, (state, action) => {
     state.books.push(action.payload);
---
+++ src/redux/categories/categories.js
@@ -0,0 +1,3 @@
+import { createReducer, createAction } from '@reduxjs/toolkit';
+
+export const CHECKING_STATUS = createAction('categories/check_staus');
