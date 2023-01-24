83b85cc6825c1640b33e58d6048f5b021eb8d9e4
felixDev22 <felixnzomo@gmail.com>
2023-01-24 03:27:36 +0300

    Set initialState to an empty arr

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,3 +1,8 @@
 import { createReducer, createAction } from '@reduxjs/toolkit';
 
 export const CHECKING_STATUS = createAction('categories/check_staus');
+
+// set intial state to empty array
+const initialState = {
+  status: '',
+};
