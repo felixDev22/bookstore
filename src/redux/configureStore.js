0b246d7a7d57e9073a0426779b14360aa2c31e05
felixDev22 <felixnzomo@gmail.com>
2023-01-24 14:03:38 +0300

    update configStore.js file arrangement

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,10 @@ import categoriesReducer from './categories/categories';
 import booksReducer from './books/books';
 
 const store = configureStore({
-  reducer: { books: booksReducer, categories: categoriesReducer },
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
 
 export default store;
