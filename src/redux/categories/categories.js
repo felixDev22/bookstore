9f94d6a4be970ba143e3d8d40cb47abee09331f1
felixDev22 <felixnzomo@gmail.com>
2023-01-24 13:56:25 +0300

    Update categories

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,4 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
+import Categories from '../../components/pages/Categories';
 
 // Define action type for addding & removing
 const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
@@ -7,15 +8,21 @@ const CREATE_CATEGORIES = 'bookstore/Categories/CREATE_CATEGORIES';
 // set intial state to empty array
 const initialState = [];
 
-// Action creator
-const categoriesReducer = createReducer(initialState, (builder) => {
-  builder.addCase(CHECKING_STATUS, (state) => {
-    // Check action
-    const currentState = { ...state, status: 'Under construction' };
+export const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
 
-    return currentState;
-  });
-  builder.addDefaultCase((state) => state);
+export const createCategories = () => ({
+  type: CREATE_CATEGORIES,
+  payload: Categories,
+});
+
+// Action creator
+const categoriesReducer = createReducer(initialState, {
+  [CHECK_STATUS]: () => 'Under construction',
+  [CREATE_CATEGORIES]: (state, action) => {
+    state.push(...action.payload);
+  },
 });
 
 export default categoriesReducer;
