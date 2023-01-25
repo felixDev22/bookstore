9a06d63ee1b90d2acafd5f4ee6f9a3f75eb9b7f0
felixDev22 <felixnzomo@gmail.com>
2023-01-25 15:01:38 +0300

    Add handleSubmt function, add author input area

--- src/components/Books/AddNewBook.js
+++ src/components/Books/AddNewBook.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import Card from '../DisplayUI/Card';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { addBook } from '../../redux/books/books';
+import Card from '../DisplayUI/Card';
 
-const AddNewBook = () => (
-  <Card>
-    <div className="form-container">
-      <h2>Add New Book</h2>
-      <form className="bookInput">
-        <div>
-          <input type="text" placeholder="Book Title" className="bookTitle" />
-        </div>
-        <div>
-          <select>
-            <option value="Adventure">Adventure</option>
-            <option value="Comedy">Comedy</option>
-            <option value="Romance">Romance</option>
-          </select>
-        </div>
-        <div>
-          <button type="button">Add Book</button>
-        </div>
-      </form>
-    </div>
-  </Card>
-);
+const AddNewBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const book = { title, author };
+    dispatch(addBook(book));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <Card>
+      <div className="form-container">
+        <h2>Add New Book</h2>
+        <form className="bookInput" onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Book Title"
+              className="bookTitle"
+              value={title}
+              required
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Author"
+              className="Author"
+              value={author}
+              required
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <select>
+              <option value="Adventure">Adventure</option>
+              <option value="Comedy">Comedy</option>
+              <option value="Romance">Romance</option>
+            </select>
+          </div>
+          <div>
+            <button type="button">Add Book</button>
+          </div>
+        </form>
+      </div>
+    </Card>
+  );
+};
 
 export default AddNewBook;
