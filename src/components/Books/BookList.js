5f898a4e95f02a09f246952471c234d005cf5d8d
felixDev22 <felixnzomo@gmail.com>
2023-01-25 16:40:55 +0300

    Add functionality for "Add new", unique id, click

--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,23 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import AddNewBook from './AddNewBook';
+import { removeBook } from '../../redux/books/books';
 
-const BookList = ({ books }) => (
-  <ul>
-    {books.map((book) => (
-      <Book
-        key={book.id}
-        id={book.id}
-        title={book.title}
-        author={book.author}
-        category={book.category}
-      />
-    ))}
-  </ul>
-);
+const BookList = () => {
+  const dispatch = useDispatch();
 
-BookList.propTypes = {
-  books: PropTypes.arrayOf.isRequired,
+  const handleDelete = (bookId) => {
+    dispatch(removeBook({ id: bookId }));
+  };
+  const bookHolder = useSelector((state) => state.book);
+
+  return (
+    <div className="bookHolder">
+      {bookHolder.map((book) => (
+        <div key={book.id} className="book-item">
+          <div className="book-title&author">
+            <Book title={book.title} author={book.author} />
+            <br />
+            <div className="comment">
+              <button type="button" className="btn">
+                Comments
+              </button>
+              <div className="line-divider" />
+              <button
+                type="button"
+                className="btn"
+                onClick={() => handleDelete(book.id)}
+              >
+                Remove
+              </button>
+              <div className="line-divider" />
+              <button type="button" className="btn">
+                Edit
+              </button>
+            </div>
+          </div>
+
+          <div className="loading">
+            <h3>54% completed</h3>
+          </div>
+          <div className="Big-divider" />
+
+          <div className="Current">
+            <p>CURRENT CHAPTER</p>
+            <p>CHAPTER 23</p>
+            <br />
+            <button type="button" className="update">
+              UPDATE PROGRESS
+            </button>
+          </div>
+        </div>
+      ))}
+      <hr />
+      <AddNewBook />
+    </div>
+  );
 };
 
 export default BookList;
