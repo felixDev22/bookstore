a1f77d3e2d02d5e7a8a38f8fdbc4e0765195bd78
felixDev22 <felixnzomo@gmail.com>
2023-01-25 15:47:53 +0300

    Update redux categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,18 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // Define constants
 const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
 const initialState = [];
 
 // Define action
-export const checkStatus = () => ({
+export const checkStatus = (status) => ({
   type: CHECK_STATUS,
+  status,
 });
 
-const info = 'Under Construction';
 // Define reducer
 const categoryReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHECK_STATUS:
-      return info;
+      return [
+        ...state,
+        {
+          id: uuidv4(),
+          text: action.status.text,
+        },
+      ];
     default:
       return state;
   }
