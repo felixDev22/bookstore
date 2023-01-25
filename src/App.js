39cf3b4f54e562fc396ad3ac4b214dac28c5991a
felixDev22 <felixnzomo@gmail.com>
2023-01-25 16:31:38 +0300

    Import navbar, add class container on App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Display from './components/DisplayUI/Display';
+import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Categories from './components/pages/Categories';
 
 import './index.css';
 
-const App = () => (
-  <Display>
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/categories"
-        element={<Categories />}
-      />
-    </Routes>
-  </Display>
-);
-
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
+}
 export default App;
