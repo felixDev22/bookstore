4664690530a7c13d14a8aba34806ef5c7b3c6a57
felixDev22 <felixnzomo@gmail.com>
2023-01-25 15:34:48 +0300

    Add an array of books to redux books.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,26 @@
-// Define action type for addding & removing
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+// set intial state to an empty array of books
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'Maasai Mara',
+    author: 'Bazu Bazu',
+  },
+  {
+    id: uuidv4(),
+    title: 'Giraffe Park',
+    author: 'Sonko Bazu',
+  },
+  {
+    id: uuidv4(),
+    title: 'Amboseli Mambo',
+    author: 'Kitu Safi',
+  },
+];
 
 export const addBook = (book) => ({
   type: ADD_BOOK,
@@ -11,9 +31,6 @@ export const removeBook = (bookId) => ({
   type: ADD_BOOK,
   payload: bookId,
 });
-
-// set intial state to an empty array of books
-const initialState = [];
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
