29b12d9786bab479cbe1b359cedf350b93a39084
felixDev22 <felixnzomo@gmail.com>
2023-01-27 13:45:12 +0300

    Add nav-item class to contain the items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,20 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <header>
     <nav>
-      <div>
-        <h2>Bookstore CMS</h2>
+      <div className="nav-items container">
+        <div>
+          <h2>Bookstore CMS</h2>
+        </div>
+        <ul>
+          <li>
+            <NavLink to="/">BOOKS</NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories">CATEGORIES</NavLink>
+          </li>
+        </ul>
+        <div>user action</div>
       </div>
-      <ul>
-        <li>
-          <NavLink to="/">BOOKS</NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories">CATEGORIES</NavLink>
-        </li>
-      </ul>
-      <div>user action</div>
     </nav>
   </header>
 );
