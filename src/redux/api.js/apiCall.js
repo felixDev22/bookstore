3e2098e79b3560b805ed0403dd5e3f983402b705
felixDev22 <felixnzomo@gmail.com>
2023-01-27 08:15:57 +0300

    Use axio to make api calls

--- src/redux/api.js/apiCall.js
+++ src/redux/api.js/apiCall.js
@@ -1,6 +1,27 @@
+const { createAsyncThunk } = require('@reduxjs/toolkit');
+const { default: axios } = require('axios');
+
 //action type
 const POST_BOOKS = 'bookstore/POST_BOOKS';
 const GET_BOOKS = 'bookstore/GET_BOOKS';
 const DELETE_BOOKS = 'bookstore/DELETE_BOOKS';
 
 const BooksID = 'C1cSMj0ZDVI91rbGgKJ9';
+
+//Get action
+const getBooks = createAsyncThunk(GET_BOOKS, async () => {
+  const endpoint = `/apps/${BooksID}/books`;
+  const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi${endpoint}`;
+  const response = await axios.get(url);
+  const bookList = Object.entries(response.data).map(([key, value]) => {
+    const { title, category, author } = value[0];
+    return {
+      Id: key,
+      title,
+      author,
+      category,
+    };
+  });
+
+  return bookList;
+});
