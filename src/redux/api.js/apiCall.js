572fd20a792d083a42c7c5496b2f548d7c31eb03
felixDev22 <felixnzomo@gmail.com>
2023-01-27 08:00:36 +0300

    Add action type and api unique id

--- src/redux/api.js/apiCall.js
+++ src/redux/api.js/apiCall.js
@@ -1,1 +1,6 @@
+//action type
+const POST_BOOKS = 'bookstore/POST_BOOKS';
+const GET_BOOKS = 'bookstore/GET_BOOKS';
+const DELETE_BOOKS = 'bookstore/DELETE_BOOKS';
 
+const BooksID = 'C1cSMj0ZDVI91rbGgKJ9';
