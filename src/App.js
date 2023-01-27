5525fa595c7837c83cc1f1d686dc79105e594019
felixDev22 <felixnzomo@gmail.com>
2023-01-27 09:51:16 +0300

    Import books to the apps.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import BookList from './components/Books/BookList';
 import Navbar from './components/Navbar';
 import Categories from './components/pages/Categories';
+import { getBooks } from './redux/api.js/apiCall';
 
 import './index.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
   return (
     <div className="container">
       <Navbar />
